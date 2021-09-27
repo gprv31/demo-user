7b479fa84b7ad5d8404945c32dbca4902cacf4b4
Giancarlo Velasquez <gian_carlo24@hotmail.com>
2021-09-27 02:41:11 -0500

    [Create] Adding util files

---
+++ src/main/java/com/example/user/util/AuthenticateUtil.java
@@ -0,0 +1,20 @@
+package com.example.user.util;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class AuthenticateUtil {
+
+  public static void authenticate(AuthenticationManager authenticationManager,
+                           String username, String password) throws Exception {
+    try {
+      authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    } catch (DisabledException e) {
+      throw new Exception("USER_DISABLED", e);
+    } catch (BadCredentialsException e) {
+      throw new Exception("INVALID_CREDENTIALS", e);
+    }
+  }
+}
