281c0ddbb9cdaaeb62d70c8611aecd008d4a2d9f
gprv31 <31750257+gprv31@users.noreply.github.com>
2021-09-27 11:31:26 -0500

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- User: admin, pass: admin
 insert into Usuario(user_name, encrypted_password, user_role, email, enabled)
 values ('admin','$2a$10$wgSabwrLSluanBZNCiXZZ.IMKCtm5PddsIwL9evYqDEdYm/LyBwlS', 'ADMIN', 'admin@mail.com', 1);
 
